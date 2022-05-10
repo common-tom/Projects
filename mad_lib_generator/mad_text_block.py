0cd4161556b4508d0c075a53904432f6627ec5c2
Tommy <104910327+common-tom@users.noreply.github.com>
2022-05-09 19:49:58 -0500

    Add files via upload

---
+++ mad_lib_generator/mad_text_block.py
@@ -0,0 +1,36 @@
+
+game = input('Want to play a game? (y/n):')
+
+if game == 'y':
+    print('ok')
+elif game == 'n':
+    print('well fuck you then')
+else:
+    print("I'm sorry I don't understand. Try 'y' or 'n'")
+    # print(game)
+
+
+
+pl_noun1 = input("Enter a plural noun: ")
+pl_noun2 = input("Enter another plural noun: ")
+pl_noun3 = input("Enter another plural noun: ")
+pl_noun4 = input("Enter another plural noun: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter another noun: ")
+verb_1 = input("Enter a verb: ")
+verb_ing = input("Enter a verb ending in -ing: ")
+
+
+
+print(f'When I go to the arcade with my {pl_noun1} there are lots of games to play.')
+print('I spend lots of time there with my friends.')
+print(f'In the game X-Men you can be different {pl_noun2}.')
+print(f'The point of the game is to {verb_1} every robot.')
+print('You also need to save people. Then you can go to the next level.')
+print(f'In the game Star Wars you are Luke Skywalker and you try to destroy every {noun1}.')
+print(f'In a car racing/motorcycle racing game you need to beat every computerized vehicle that you are {verb_ing} against.')
+print('There are a whole lot of other cool games.')
+print(f'When you play some games you win {pl_noun3} for certain scores.')
+print(f"Once you're done you can cash in your tickets to get a big {noun2}.")
+print(f"You can save your {pl_noun4} for another time.")
+print("When I went to this arcade I didn't believe how much fun it would be. So far I have had a lot of fun every time I've been to this great arcade!")
